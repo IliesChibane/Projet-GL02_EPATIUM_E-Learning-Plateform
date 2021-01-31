b428c0652e3e9f303e975f4045d3ecc1b869f664
ilies <ilies.chibane.2000@gmail.com>
2021-01-31 11:15:02 +0100

    Emploie du temps et notes

---
+++ src/sample/EnseignantsEtudiantController.java
@@ -0,0 +1,52 @@
+package sample;
+
+import Classes.Enseignant;
+import Classes.Etudiant;
+import Classes.Utilisateur;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
+
+public class EnseignantsEtudiantController implements Initializable {
+
+    @FXML
+    private TableView ListeEnseignant;
+    @FXML
+    private TableColumn<Enseignant, String> Matriculle;
+    @FXML private TableColumn<Enseignant, String> Nom;
+    @FXML private TableColumn<Enseignant, String> Prenom;
+    @FXML private TableColumn<Enseignant, String> mail;
+
+    Utilisateur u = new Utilisateur();
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ListeEnseignant.setItems(null);
+        LinkedList<Enseignant> lle = null;
+        try {
+            lle = Etudiant.getListeEnseignant(Etudiant.getModule(Etudiant.getSectionE(u.getIdd())));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        ObservableList<Enseignant> le = FXCollections.observableArrayList();
+        for(Enseignant e : lle)
+        {
+            le.add(e);
+            Matriculle.setCellValueFactory(cellData-> cellData.getValue().idProperty());
+            Nom.setCellValueFactory(cellData-> cellData.getValue().nomProperty());
+            Prenom.setCellValueFactory(cellData-> cellData.getValue().prenomProperty());
+            mail.setCellValueFactory(cellData-> cellData.getValue().emailProperty());
+            ListeEnseignant.setItems(le);
+        }
+    }
+}
